29e574cb4e7bd6a41552c480b36b4748f5d2a340
Revathi <revathimarappan5@gmail.com>
2025-05-23 16:05:37 +0530

    day 9

--- Desktop/day 7/ananthi/thulasi/urls.py
+++ Desktop/day 7/ananthi/thulasi/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 
 urlpatterns=[
-    path('home/',homepage)
-]
+    path('home/',homepage),
+    path('student_form/', student_form, name='student_form'), # Add a name here
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    ]
+ 
