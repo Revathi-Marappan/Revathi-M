cb9cd589f0c65f1e16fa57ef4246de16cc1706d5
Revathi <revathimarappan5@gmail.com>
2025-05-30 12:23:26 +0530

    day 15

--- Desktop/full stack project/moodify/views.py
+++ Desktop/full stack project/moodify/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
